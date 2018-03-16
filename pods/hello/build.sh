c87ed8f9fe52791a8f4e3f8d7b5035cde858a0ab
Arief Darmawan <arief@eaciit.com>
2018-03-16 14:54:59 +0700

    add docker

--- pods/hello/build.sh
+++ pods/hello/build.sh
@@ -1,3 +1,4 @@
-GOOS=linux GOARCH=arm go build -o ../../docker/go-hello/hello
+GOOS=linux go build -o ../../docker/go-hello/hello
+docker rmi -f ariefdarmawan/go-hello
 docker build --rm -t ariefdarmawan/go-hello:latest ../../docker/go-hello
 docker push ariefdarmawan/go-hello
